3b94bf755566d16984b2132f9d75892a9d99eb09
NightFrost1337 <90924656+NightFrost1337@users.noreply.github.com>
2021-09-19 14:19:27 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+Base Discord Bot
+
+This is a Discord Bot Template. You can add your own commands to it by creating a ``command.js`` in the ``commands`` folder. The name of that file is also the command name!
+
+## Installation
+
+Use the package manager [npm](https://nodejs.org/en/download/) to install all needed modules.
+
+```bash
+npm install
+```
+
+Your can get the Token from the Discord Developer Page. [Link](https://discord.com/developers/applications)
+
+## Included
+- Command Manager
+- Event Manager
+- Ping Command
+- Needed Events
+
---
+++ index.js
@@ -0,0 +1,44 @@
+//create by Night Frost / NightFrost <./>#0001 / all credits to NightFrost
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const fs = require("fs");
+
+const Enmap = require("enmap");
+
+const config = require("./config.json");
+client.config = config;
+
+client.commands = new Enmap();
+
+console.log("------------------------------------------------");
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error;
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const evt = require(`./events/${file}`);
+    let evtName = file.split(".")[0];
+    console.log(`Loaded event '${evtName}'`);
+    client.on(evtName, evt.bind(null, client));
+  });
+  console.log("------------------------------------------------");
+});
+
+fs.readdir("./commands/", async (err, files) => {
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let cmdName = file.split(".")[0];
+    console.log(`Loaded Command '${cmdName}'`);
+    client.commands.set(cmdName, props);
+  });
+  console.log("------------------------------------------------");
+});
+
+process.on("unhandledRejection", (error) => {
+  console.error("Unhandled promise rejection:", error);
+});
+
+client.login(config.token || process.env.TOKEN);
