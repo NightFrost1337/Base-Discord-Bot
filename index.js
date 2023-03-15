b4ddd74529841167ad1a3de29ac7771dbdca510e
NightFrost <90924656+NightFrost1337@users.noreply.github.com>
2023-03-15 23:01:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-//create by Night Frost / NightFrost <./>#0001 / all credits to NightFrost
-
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const fs = require("fs");
-
 const Enmap = require("enmap");
 
 const config = require("./config.json");
@@ -14,26 +11,37 @@ client.commands = new Enmap();
 
 console.log("------------------------------------------------");
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error;
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    const evt = require(`./events/${file}`);
-    let evtName = file.split(".")[0];
-    console.log(`Loaded event '${evtName}'`);
-    client.on(evtName, evt.bind(null, client));
+fs.readdir("./events", (err, eventFiles) => {
+  if (err) console.error;
+  
+  eventFiles.forEach((eventFile) => {
+    if (!eventFile.endsWith(".js")) return;
+
+    const eventName = eventFile.split(".")[0];
+    const event = require(`./events/${eventFile}`);
+
+    console.log(`Loaded event '${eventName}'`);
+
+    client.on(eventName, (...args) => event(client, ...args));
   });
+
   console.log("------------------------------------------------");
 });
 
-fs.readdir("./commands/", async (err, files) => {
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let cmdName = file.split(".")[0];
-    console.log(`Loaded Command '${cmdName}'`);
-    client.commands.set(cmdName, props);
+fs.readdir("./commands", (err, commandFiles) => {
+  if (err) console.error;
+  
+  commandFiles.forEach((commandFile) => {
+    if (!commandFile.endsWith(".js")) return;
+
+    const commandName = commandFile.split(".")[0];
+    const props = require(`./commands/${commandFile}`);
+
+    console.log(`Loaded command '${commandName}'`);
+
+    client.commands.set(commandName, props);
   });
+
   console.log("------------------------------------------------");
 });
 
